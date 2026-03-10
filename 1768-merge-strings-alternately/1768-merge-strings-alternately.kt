8b6e2108098be24e05020509b6456fda6a6d8820
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-03-10 16:58:56 +0900

    Time: 95 ms (70.63%), Space: 40.5 MB (56.47%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun mergeAlternately(word1: String, word2: String): String {
+        //return할 결과값 선언
+        val result = StringBuilder()
+
+        //최대 길이값 선언
+        val MaxLength = maxOf(word1.length, word2.length)
+
+        for(i in 0..MaxLength-1){
+            //for(i in 0 until MaxLength) 이렇게 써도됨 
+            if(i < word1.length)
+                result.append(word1[i])
+        
+        if(i < word2.length)
+                result.append(word2[i])
+        }
+    
+    return result.toString()
+    }
+}
