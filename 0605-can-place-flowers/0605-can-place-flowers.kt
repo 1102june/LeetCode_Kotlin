28954875acdb084b09f55973fedc0cb5ed3deaa6
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-03-16 20:01:49 +0900

    Time: 2 ms (66.29%), Space: 48 MB (86.14%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.kt
+++ 0605-can-place-flowers/0605-can-place-flowers.kt
@@ -1,34 +1,22 @@
 class Solution {
     fun canPlaceFlowers(flowerbed: IntArray, n: Int): Boolean {
-        // 1. 심을 꽃이 0개면 화단을 볼 필요도 없이 성공! (빠른 퇴근)
+        var count = 0
+        //1. for문을 돌아
         if (n == 0) return true
-        
-        var planted = 0 // 내가 심은 꽃의 개수
-        
-        // 2. 화단을 0번부터 끝까지 걸어갑니다 (이게 Greedy의 시작)
-        for (i in 0 until flowerbed.size) {
-            
-            // 3. 내 자리가 비어있을 때만 양옆을 살핍니다
-            if (flowerbed[i] == 0) {
-                
-                // 4. 왼쪽, 오른쪽이 비어있는지(또는 벽인지) 확인
-                val emptyLeft = (i == 0 || flowerbed[i - 1] == 0)
-                val emptyRight = (i == flowerbed.size - 1 || flowerbed[i + 1] == 0)
-                
-                // 5. 심을 수 있는 완벽한 조건이라면? 
-                if (emptyLeft && emptyRight) {
-                    flowerbed[i] = 1 // 당장 심는다! (이 행동이 Greedy의 핵심)
-                    planted += 1     // 심은 개수 추가
-                    
-                    // 6. 목표량을 다 채웠으면 남은 화단은 검사할 필요도 없이 성공!
-                    if (planted >= n) {
-                        return true
-                    }
-                }
+        for (i in 0 until flowerbed.size){
+            //flowerbed의 i번째 인덱스가 0일때
+            if(flowerbed[i] == 0){  //i번 인덱스 값이 0 이면 
+                var leftempty = (i==0 || flowerbed[i-1]==0)  //그 기준 좌측값이 0이거나 범위 안인지 확인
+                var rightempty = (i == flowerbed.size-1 || flowerbed[i+1]==0)
+            if (leftempty && rightempty) {  //둘다 만족한다면 해당 i번째 인덱스에 심고 
+                flowerbed[i] = 1
+                count ++
             }
         }
+        }
+        if (count >= n)
+            return true
+            else return false
         
-        // 끝까지 다 돌았는데도 목표량을 못 채웠으면 실패
-        return planted >= n
     }
 }
