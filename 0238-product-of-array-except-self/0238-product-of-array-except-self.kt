3de998943d62480653f5d8ece50312900c7de023
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-04-03 20:51:14 +0900

    Time: 2 ms (99.61%), Space: 58.9 MB (97.43%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.kt
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.kt
@@ -1,21 +1,21 @@
 class Solution {
     fun productExceptSelf(nums: IntArray): IntArray {
+        //1. 정수형 배열 [1,1,1,1... ] 구조 정의
         val n = nums.size
-        
-        val result = IntArray(n){ 1 } //결과 담을 배열 생성
+        val arr = IntArray(n){ 1 }
 
-        // 기준으로 오른쪽 값 다 곱해서 result에 집어넣기
-        var leftMulti = 1
+        var leftpointer = 1
+        //leftpointer를 기준으로 오른쪽에 있는 인덱스 다 곱하기
         for (i in 0 until n){
-            result[i] = leftMulti
-            leftMulti = leftMulti * nums[i]
+            arr[i] = leftpointer
+            leftpointer = leftpointer*nums[i]
         }
-
-        var rightMulti = 1
-        for(i in n-1 downTo 0){
-            result[i] = result[i] * rightMulti
-            rightMulti = rightMulti * nums[i]
-        } 
-        return result
+        var rightpointer = 1 
+        //rightpointe를 기준으로 왼쪽에 있는 인덱스 다 곱하기 
+        for (j in n-1 downTo 0){ //downTo
+            arr[j] = arr[j] * rightpointer
+            rightpointer = rightpointer * nums[j]
+        }
+        return arr
     }
 }
