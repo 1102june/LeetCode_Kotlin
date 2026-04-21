95d014168ed1689f97e331f3c01b2af1ff61570a
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-04-21 16:37:51 +0900

    Time: 0 ms (100%), Space: 41.9 MB (85.1%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.kt
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.kt
@@ -1,18 +1,19 @@
 class Solution {
     fun gcdOfStrings(str1: String, str2: String): String {
-    //패턴이 있는지 어떻게 알아낼것인가
-    //1. 완전탐색 방식 - str1과 str2를 비교해서 문자개수 구해서 합치기
-    //2. GCD 방식 - str1,str2를 합쳤을때의 패턴이 같은가
-    var maxnum = 0 //가변상수 var
-    if (str1+str2 == str2+str1){
-        for (i in 1..minOf(str1.length, str2.length)){   //두 문자열 중 더 짧은 
-            if(str1.length % i == 0 && str2.length % i == 0) { 
-                // 최대공약수 만족하는 조건 
-                maxnum = i
-            }
+        // 문제를 보고 GCD최대공약수 활용 못하면 나가리.
+        //합체순서를 다르게 했을때. 
+        if(str1+str2 != str2+str1){
+            return "" 
+    }
+    var a = str1.length
+    var b = str2.length
+
+    while (b != 0) {
+            val temp = b
+            b = a % b  // b에는 나머지를 넣고
+            a = temp   // a에는 아까의 b를 넣음
         }
-    }
-    else return ""
-    return str1.substring(0,maxnum)   //substring으로 자르기 
-    }
+
+    return str1.substring(0,a)
+}
 }
