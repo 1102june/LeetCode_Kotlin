8a826dbbb5433d184ae9d094ec17cc445ff30a92
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-04-22 20:16:26 +0900

    Time: 2 ms (86.44%), Space: 51.2 MB (45.37%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.kt
+++ 0283-move-zeroes/0283-move-zeroes.kt
@@ -1,16 +1,17 @@
 class Solution {
     fun moveZeroes(nums: IntArray): Unit {
-        var write = 0 
+        var pt2 = 0
 
-        for (read in 0 until nums.size){
-            if(nums[read] != 0){
-                nums[write] = nums[read]
-                write++
+        for(pt1 in 0 until nums.size){ 
+            //pt1이 가리키는 인덱스가 0이 아닌 숫자를 pt2인덱스 자리에 꽂음
+            if(nums[pt1]!=0){
+                nums[pt2] = nums[pt1]
+                pt2++
             } 
+        } 
+        while(pt2 < nums.size){ //pt2가 가리키는 곳에서부터 끝까지 0으로 채움
+            nums[pt2] = 0
+            pt2++
         }
-    while (write<nums.size){
-        nums[write] = 0 
-        write ++
-    }
     }
 }
