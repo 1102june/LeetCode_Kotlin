080a4f496b156e783b9addb2a4d6c90b15fd93b1
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-04-27 12:55:52 +0900

    Time: 3 ms (99.26%), Space: 61.3 MB (69.31%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.kt
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.kt
@@ -1,19 +1,20 @@
 class Solution {
     fun findMaxAverage(nums: IntArray, k: Int): Double {
-        var currentSum = 0.0
-        //0번 인덱스부터 4개 더함 
-        for (k in 0 until k){
-            currentSum+=nums[k]
-        }
+    
+    var current = 0.0
+    var max = 0.0
 
-        var maxSum = currentSum
+    for (i in 0 until k){
+        current += nums[i]
+    }
+    max = current
 
-        for (i in k until nums.size){ 
-            currentSum = currentSum + nums[i] - nums[i-k] 
-            if (maxSum < currentSum) { 
-                maxSum = currentSum
-            }
-        }  
-        return maxSum / k
+    for (j in k until nums.size){
+        current = current - nums[j-k] +nums[j]
+        if(current >  max)
+            max = current 
+    }
+    return (max/k)
+        
     }
 }
