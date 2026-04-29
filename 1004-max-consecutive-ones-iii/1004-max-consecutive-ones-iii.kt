e8008c26a1ab211dbee723de72dd00c886992201
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-04-29 21:22:50 +0900

    Time: 6 ms (57.23%), Space: 57.7 MB (45.85%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.kt
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.kt
@@ -1,21 +1,22 @@
 class Solution {
     fun longestOnes(nums: IntArray, k: Int): Int {
-        var left = 0
-        var zero = 0
+        var a = 0 
+        var b = 0
         var maxLen = 0
+        var zero = 0
 
-        for(right in nums.indices){ //right = 0,1,2..
-            if(nums[right] == 0){  //그냥 right는 0 만나면 Zero++
+        for (a in nums.indices){ // a= 0,1,2,3..
+            if(nums[a] == 0){
                 zero++
             } 
-            while(zero > k){  //zero의 개수가 k보다 큰경우에만 수행
-                if(nums[left] == 0){
-                    zero--    //left는 0번인덱스부터 출발, 만나면 Zero--
+            while(zero > k){
+                if(nums[b] == 0){
+                    zero--
                 } 
-                left++
-            }
-            maxLen = max(maxLen, right-left+1)  
-        }  
-        return maxLen 
+                b++
+            } 
+            maxLen = max(maxLen, a-b+1) //a가 더 앞으로 달려감
+        }
+        return maxLen
     }
 }
