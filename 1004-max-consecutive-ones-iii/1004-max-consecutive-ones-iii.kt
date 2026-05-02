c8e3042852e8833c67452ef246473f7216edff20
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-05-02 19:05:34 +0900

    Time: 5 ms (92.31%), Space: 57.4 MB (68.27%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.kt
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.kt
@@ -1,22 +1,26 @@
 class Solution {
     fun longestOnes(nums: IntArray, k: Int): Int {
+        // k개수 만큼 0을 1로 Flip가능, 그때 연속된 1의 최대길이
         var a = 0 
-        var b = 0
-        var maxLen = 0
+        var b = 0 
+        var maxLen = 0 
         var zero = 0
 
-        for (a in nums.indices){ // a= 0,1,2,3..
+        for(a in nums.indices){
             if(nums[a] == 0){
-                zero++
+                zero++ 
+                //a는 처음부터 그냥 출발하여 0의 개수를 Count하면서 지나감
             } 
             while(zero > k){
                 if(nums[b] == 0){
-                    zero--
-                } 
+                    zero--               
+                }
                 b++
+                //b는 a가 센 0의개수가 k의 수보다 많은 조건에만 발동
+                // 0인경우를 찾아서 빼버림
             } 
-            maxLen = max(maxLen, a-b+1) //a가 더 앞으로 달려감
-        }
+            maxLen = max(maxLen, a-b+1)
+        } 
         return maxLen
     }
 }
