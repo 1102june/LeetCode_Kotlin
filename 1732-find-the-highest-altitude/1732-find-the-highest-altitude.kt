6bc02b4c9a4190cf7d8a1e8a0d11ef95c41c7903
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-05-10 14:57:54 +0900

    Time: 0 ms (100%), Space: 40.9 MB (67.95%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.kt
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.kt
@@ -1,17 +1,18 @@
 class Solution {
     fun largestAltitude(gain: IntArray): Int {
-        // start altitude = 0
         var max = 0
-        if(gain[0]> max){
+        if(gain[0] > max){
             max = gain[0]
         }
+        //처음엔 i+1로 돌리려 했는데 그러니까 outofBounds떠서 수정
+        //만약 0번인덱스에서 최대 altitude 나오는 경우를 따지지를 않는 상황 발생
         for(i in 1 until gain.size){
-            gain[i] = gain[i-1] + gain[i] // 다음 고도값이랑 현고도값이랑 합해서 넣기
-            if(gain[i] > max){  //고도값이 이전보다 높아지면 교체
+            gain[i] = gain[i]+gain[i-1]
+            if(gain[i] > max){
                 max = gain[i]
-            } 
-        } 
+            }
+        }
         return max
-        
+         
     }
 }
