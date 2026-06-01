a0b99d1e021e60a30edac24521f30e82ad5c3e23
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-01 14:49:37 +0900

    Time: 96 ms (60.75%), Space: 40.9 MB (39.02%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.kt
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.kt
@@ -1,19 +1,16 @@
 class Solution {
     fun mergeAlternately(word1: String, word2: String): String {
-        // 복습
         val result = StringBuilder()
         val max = maxOf(word1.length, word2.length)
 
         for(i in 0 until max){
-
             if(i < word1.length){
                 result.append(word1[i])
-            } 
-            if (i < word2.length){
+            }
+            if(i < word2.length){
                 result.append(word2[i])
             }
-        }
+        } 
         return result.toString()
-        
     }
 }
