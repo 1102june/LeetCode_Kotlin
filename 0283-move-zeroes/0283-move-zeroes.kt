e77a1f99d155585d582ea87756f7bb8740a55e4c
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-02 13:41:47 +0900

    Time: 2 ms (86.7%), Space: 49.4 MB (90.2%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.kt
+++ 0283-move-zeroes/0283-move-zeroes.kt
@@ -1,17 +1,20 @@
 class Solution {
     fun moveZeroes(nums: IntArray): Unit {
-        var pt2 = 0   //최종적으로 이위치부터 0 채울거임
-        
-        for(pt1 in 0 until nums.size){
-            if(nums[pt1] != 0){
-                nums[pt2] = nums[pt1]
-                pt2++
-            } 
-        } 
+        //만약 nums의 길이가 1이면 [0] return하는 예외처리
+        if(nums.size == 1){
+            return 
+        }
+        var zero = 0 //0이 들어있는 위치의 인덱스값저장
 
-        while(pt2 < nums.size){
-            nums[pt2] = 0
-            pt2++
-        } 
+        for(i in 0 until nums.size){
+            if(nums[i] != 0){
+                val temp = nums[zero]
+                nums[zero] = nums[i]
+                nums[i] = temp
+                
+                zero++
+            }
+        }
+        //return type이 unit이면 void와같은거임, return 할필요 x
     }
 }
