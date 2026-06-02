c4b757f906500069cfa60289fca38f3bb50ace2b
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-02 12:49:26 +0900

    Time: 20 ms (57.24%), Space: 45.9 MB (40%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.kt
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.kt
@@ -1,35 +1,16 @@
 class Solution {
     fun reverseWords(s: String): String {
-        val arr = s.toCharArray()
+        //앞뒤 공백제거, 연속된 공백제거
+        val a = s.trim().split(" ").filter{ it.isNotEmpty() }
+
         val result = StringBuilder()
 
-        var pointer1 =  arr.size-1
-        var pointer2 =  arr.size-1
-        
-        for(i in 0 until arr.size){  //길이만큼 수행
-            if(arr[pointer1] != ' '){ //해당 인덱스가 공백이 아니면 왼쪽으로 한칸이동
-                pointer1--
+        for(i in a.size-1 downTo 0){
+            result.append(a[i])
+            if(i != 0){ //마지막에 공백추가되지않도록 예외처리
+                result.append(" ") // 단어박스 하나 집어넣고 공백추가
             }
-            else { // 공백일경우.
-            
-                if(pointer1 < pointer2) {  //연속공백인 상황을 배제
-                    for(j in (pointer1 + 1)..pointer2){  
-                        result.append(arr[j]) 
-                    }
-                    result.append(' ') // 단어 뒤에 공백 한칸 추가
-                }
-                
-                pointer2 = pointer1 - 1 
-                pointer1--
-            } 
-        }
-
-        // OutOfBounds 때문에 범위를 벗어나는 상황 발생. 첫단어 잘림
-        if (pointer1 < pointer2) {
-            for(j in (pointer1 + 1)..pointer2) {
-                result.append(arr[j])
-            }
-        }
-        return result.toString().trim()
+        } 
+        return result.toString()
     }
 }
