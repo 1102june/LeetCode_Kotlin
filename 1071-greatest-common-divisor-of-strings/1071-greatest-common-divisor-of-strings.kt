341b60537693e1fcd30708751064441dcde0c47b
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-02 11:57:22 +0900

    Time: 0 ms (100%), Space: 42.4 MB (60.75%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.kt
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.kt
@@ -8,10 +8,11 @@ class Solution {
         var a = str1.length  // 6
         var b = str2.length // 3
 
-        while(b!=0){   //유클리드 호제법
-            val r = a % b
-            a = b
-            b = r
+        while(b!=0){   //유클리드 호제법활용한 gcd 구하기
+        // a= 12 b= 8이라고 가정 
+            val r = a % b  //r = 4
+            a = b // a = 8
+            b = r // b = 4
         }
         val gcd = a
         return str1.substring(0,gcd) // substring활용해서 자르기 
