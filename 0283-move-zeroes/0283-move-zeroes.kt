795d6e5355fa432fa369c6f3537eacafcc3bbc37
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-04 14:09:15 +0900

    Time: 2 ms (86.62%), Space: 49.8 MB (75.11%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.kt
+++ 0283-move-zeroes/0283-move-zeroes.kt
@@ -1,19 +1,18 @@
 class Solution {
     fun moveZeroes(nums: IntArray): Unit {
+        var n_zero = 0
         if(nums.size == 1){
-            return //예외처리
-        }
-        var insert = 0 //삽입위치
+            return
+        } 
 
-        for (i in 0 until nums.size){
-            if(nums[i] != 0 ){
-                val temp = nums[i]
-                nums[i] = nums[insert]
-                nums[insert] = temp
-
-                insert++
+        for(i in 0 until nums.size){
+            if(nums[i] != 0){
+                var temp = nums[i]
+                    nums[i] = nums[n_zero]
+                    nums[n_zero] = temp
+                
+                n_zero++
             }
-        }
-        return
+        } 
     }
 }
