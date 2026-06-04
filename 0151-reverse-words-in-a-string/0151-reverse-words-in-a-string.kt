f95e0492a6940c853bb4abbc90a662e6693c997b
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-04 13:51:08 +0900

    Time: 20 ms (55.79%), Space: 46.1 MB (30.09%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.kt
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.kt
@@ -1,14 +1,12 @@
 class Solution {
     fun reverseWords(s: String): String {
-        //앞뒤 공백제거, 연속된 공백제거
-        val a = s.trim().split(" ").filter{ it.isNotEmpty() }
-
-        val result = StringBuilder()
+        val a = s.trim().split(" ").filter{it.isNotEmpty()}
+        var result = StringBuilder() //어떻게 선언해야하는가
 
         for(i in a.size-1 downTo 0){
             result.append(a[i])
-            if(i != 0){ //마지막에 공백추가되지않도록 예외처리
-                result.append(" ") // 단어박스 하나 집어넣고 공백추가
+            if(i != 0 ){ 
+                result.append(" ")
             }
         } 
         return result.toString()
