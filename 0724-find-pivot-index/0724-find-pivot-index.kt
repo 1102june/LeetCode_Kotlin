ea3a096b7060817b593f6d18b7a5b519cf577792
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-05 09:30:26 +0900

    Time: 1 ms (100%), Space: 46.5 MB (99.6%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.kt
+++ 0724-find-pivot-index/0724-find-pivot-index.kt
@@ -1,23 +1,20 @@
 class Solution {
     fun pivotIndex(nums: IntArray): Int {
-        var rpivot = 0 //right pivot
-        // 0번 인덱스 제외하고 다 더하기
+        var lpivot = 0
+        var rpivot = 0
         for(i in 1 until nums.size){
-            rpivot += nums[i]
+            rpivot+=nums[i] 
         }
-        //0번 인덱스, 끝번 인덱스 제외 다 더한값이 0이면 0 return
         if(rpivot == 0){
             return 0
-        } 
-        var lpivot = 0 //leftpivot
-
+        }  
         for(j in 1 until nums.size){
-            lpivot += nums[j-1]  //leftpivot에는 피봇값 점점추가
-            rpivot -= nums[j] //rightpivot에는 피봇값 점점삭제
-            if(lpivot == rpivot){ //좌측 우측 피봇값 같으면 해당 인덱스값 i return
+            lpivot+=nums[j-1]
+            rpivot-=nums[j]
+            if(lpivot == rpivot){
                 return j
-            } 
+            }
         }
-        return -1 
+        return -1
     }
 }
