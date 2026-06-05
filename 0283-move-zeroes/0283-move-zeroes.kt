a92a748d082ddfb77935892e6ed219157691e753
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-05 09:20:14 +0900

    Time: 2 ms (86.91%), Space: 51.6 MB (39.42%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.kt
+++ 0283-move-zeroes/0283-move-zeroes.kt
@@ -1,18 +1,18 @@
 class Solution {
     fun moveZeroes(nums: IntArray): Unit {
-        var n_zero = 0
         if(nums.size == 1){
-            return
-        } 
-
+            return 
+        }
+        var pointer = 0
         for(i in 0 until nums.size){
             if(nums[i] != 0){
                 var temp = nums[i]
-                    nums[i] = nums[n_zero]
-                    nums[n_zero] = temp
-                
-                n_zero++
-            }
+                nums[i] = nums[pointer]
+                nums[pointer] = temp
+
+                pointer++
+            } 
         } 
+        return
     }
 }
