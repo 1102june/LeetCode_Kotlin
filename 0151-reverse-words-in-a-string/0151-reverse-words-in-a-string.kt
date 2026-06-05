3370d09248d3930db636ae586c58f1cb5596c781
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-05 09:00:07 +0900

    Time: 22 ms (37.9%), Space: 45.5 MB (61.42%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.kt
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.kt
@@ -1,14 +1,15 @@
 class Solution {
     fun reverseWords(s: String): String {
         val a = s.trim().split(" ").filter{it.isNotEmpty()}
-        var result = StringBuilder() //어떻게 선언해야하는가
-
+//양쪽 공백, split을 활용해서 empty 공백인경우를 제외하고 잘라서 배열에 담기
+        val result = StringBuilder()
         for(i in a.size-1 downTo 0){
             result.append(a[i])
-            if(i != 0 ){ 
-                result.append(" ")
-            }
+        if(i != 0){
+            result.append(" ")
         } 
+    }
         return result.toString()
+
     }
 }
