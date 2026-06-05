6339a241b45a035d40afcab9d24deca363322dde
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-05 09:07:08 +0900

    Time: 1 ms (97.14%), Space: 40.7 MB (59.48%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.kt
+++ 0392-is-subsequence/0392-is-subsequence.kt
@@ -1,18 +1,15 @@
 class Solution {
     fun isSubsequence(s: String, t: String): Boolean {
-        if (s.length == 0){
-            return true
-        }
-        var pointer = 0
+        if(s.isEmpty()) return true 
+        var pointer = 0 
+
         for(i in 0 until t.length){
             if(s[pointer] == t[i]){
                 pointer++
-                if(pointer == s.length){
-                    return true
-                }
-            }
+                if(pointer == s.length)
+                return true 
+            } 
         }
         return false 
-
     }
 }
