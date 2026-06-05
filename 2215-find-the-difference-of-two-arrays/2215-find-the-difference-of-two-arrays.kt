16be862706bd9ddf481ca9f367657b3f1d4e45e2
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-05 10:46:51 +0900

    Time: 34 ms (36.03%), Space: 52.1 MB (40.37%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.kt
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.kt
@@ -1,11 +1,12 @@
 class Solution {
     fun findDifference(nums1: IntArray, nums2: IntArray): List<List<Int>> {
-        var set1 =  nums1.toSet()
-        var set2 = nums2.toSet()   //중복제거를 위한 set으로 변환
+        var a = nums1.toSet() 
+        var b = nums2.toSet()
 
-        var fset1 = (set1-set2).toList()
-        var fset2 = (set2-set1).toList() //차집합으로 해결한 뒤 List변환
+        val a1 = (a-b).toList()
+        val b1 = (b-a).toList()
 
-        return listOf(fset1,fset2)
+        return listOf(a1,b1)
+        
     }
 }
