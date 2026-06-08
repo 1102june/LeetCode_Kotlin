26f30a26b3570aef09c7a79ab626f5833ca9f3e6
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-08 10:56:23 +0900

    Time: 155 ms (5.61%), Space: 52.1 MB (29.91%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.kt
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.kt
@@ -1,25 +1,22 @@
 class Solution {
     fun closeStrings(word1: String, word2: String): Boolean {
-        //1. 문자열 길이가 다르면 false 예외처리
+        //swap, transform 
+        //1. 예외처리: string의 길이가 다르면 false return
         if(word1.length != word2.length){
             return false
         } 
-        //2. 문자 개수대로 map만들기
-        val map1 = word1.toList().groupingBy{it}.eachCount()
-        val map2 = word2.toList().groupingBy{it}.eachCount()
+        // 문자 개수대로 map만들기
+        val a1 = word1.toList().groupingBy{it}.eachCount()
+        val b1 = word2.toList().groupingBy{it}.eachCount()
 
-        //3. key값 추출
-        val map1_keys = map1.keys
-        val map2_keys = map2.keys
-        //4. value값 추출 -> sorteㅇ 되어있어야함
-        val map1_values = map1.values.sorted()
-        val map2_values = map2.values.sorted()
+        //keys, values 따로
+        val a2 = a1.keys.sorted()
+        val b2 = b1.keys.sorted()
 
-        if(map1_keys == map2_keys && map1_values == map2_values) {
-            return true
-        }
-        else 
-            return false
+        val a3 = a1.values.sorted()
+        val b3 = b1.values.sorted()
+
+        return (a2==b2 && a3==b3)  //결론적으로 a2,b2 와 a3,b3가 완전히 같아야함
 
     }
 }
