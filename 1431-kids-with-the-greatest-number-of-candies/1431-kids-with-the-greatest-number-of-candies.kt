e7a7ae51031d326d853f481c1cf8cffe480ef9c9
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-08 12:10:34 +0900

    Time: 10 ms (69.68%), Space: 45.9 MB (34.57%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.kt
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.kt
@@ -3,10 +3,10 @@ class Solution {
         //extra candy를 부여했을때 해당 Array에서 최대값인지 True, false
         //새로운 List선언
         val result = mutableListOf<Boolean>()
-        //최대캔디개수 찾기 -> maxof로 찾는게 가능함?
-        val sort = candies.sorted()
+        val max = candies.maxOrNull()!!
+        /*val sort = candies.sorted()
         val max = sort[candies.size-1]
-
+*/
         for(i in 0 until candies.size){
             if(candies[i]+extraCandies >= max){
                 result.add(true)
