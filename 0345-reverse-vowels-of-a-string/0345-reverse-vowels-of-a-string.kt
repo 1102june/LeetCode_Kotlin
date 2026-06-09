29a0c4ce89b05db79b4c6b2d858b7fdb0e242152
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-09 16:34:32 +0900

    Time: 10 ms (90.7%), Space: 46.2 MB (89.37%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.kt
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.kt
@@ -1,29 +1,26 @@
 class Solution {
     fun reverseVowels(s: String): String {
-        //0. String으론 계산이 불가능하니까 Char 배열로 변경
-        val arr = s.toCharArray()
-        //1. 모음 모아둔 변수설정
         val vowels = "aeiouAEIOU"
-        //2. pointer1, pointer2 정의
+        val arr = s.toCharArray() //string을 CharArray로 수정
         var pointer1 = 0
-        var pointer2 = arr.size-1
+        var pointer2 = s.length-1
 
-            while(pointer1 < pointer2) {
+        while(pointer1 < pointer2){
             if(arr[pointer1] !in vowels){
                 pointer1++
-            } 
+                }
             else if(arr[pointer2] !in vowels){
-                pointer2-- 
-        }
-        else{
-            val temp = arr[pointer1]
-            arr[pointer1] = arr[pointer2]
-            arr[pointer2] = temp
+                pointer2--
+            }
+            else if(arr[pointer1] in vowels && arr[pointer2] in vowels){
+                val temp = arr[pointer1]
+                arr[pointer1] = arr[pointer2]
+                arr[pointer2] = temp
 
-            pointer1++
-            pointer2--
+                pointer1++
+                pointer2--
+            }  
         }
+        return String(arr)
     }
-    return String(arr)
-}
 }
