4a5025c01536f76dc54687514828fa3f041d1364
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-10 12:59:49 +0900

    Time: 11 ms (87.54%), Space: 47 MB (66.89%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.kt
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.kt
@@ -1,26 +1,29 @@
 class Solution {
     fun reverseVowels(s: String): String {
-        //stringbuilder x
-        val a = s.toCharArray() // char형 배열로 변경
         val vowels = "aeiouAEIOU"
+        var left = 0
+        var right = s.length-1
+        var a = s.toCharArray()
 
-        var left = 0 
-        var right = s.length -1 
+        if(s.length == 1){  //예외처리
+            return s
+        }
         while(left < right){
             if(a[left] !in vowels){
                 left++
-            } 
+            }
             else if(a[right] !in vowels){
                 right--
-            } 
-            else if(a[left] in vowels && a[right] in vowels){
+            }  
+            if(a[left] in vowels && a[right] in vowels){
                 val temp = a[left]
                 a[left] = a[right]
                 a[right] = temp
-                left++
+
                 right--
-            }  
-        }
+                left++
+            }
+        }  
         return String(a)
     }
 }
