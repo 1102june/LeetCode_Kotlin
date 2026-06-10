4ad9a3b020c59cf827584f339ad52a61ab307807
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-10 13:04:22 +0900

    Time: 1 ms (100%), Space: 50.8 MB (46.68%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.kt
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.kt
@@ -2,26 +2,26 @@ class Solution {
     fun twoSum(numbers: IntArray, target: Int): IntArray {
         val result = IntArray(2)
         if(numbers.size == 2){
-            result[0]=1
-            result[1]=2
+            result[0] = 1
+            result[1] = 2
         } 
         var left = 0
         var right = numbers.size-1
 
-        while(left<right){
-            val sum = numbers[left] + numbers[right] 
+        while(left <  right){
+            val sum = numbers[left] + numbers[right]
             if(sum < target){
                 left++
             }
             else if(sum > target){
                 right--
             } 
-            else{
+            else if(sum==target) {
                 result[0] = left+1
                 result[1] = right+1
                 break
             }
         }
-        return result 
+        return result
     }
 }
