1df459c94ec7edac1957273f94f4bd5cf076496a
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-11 15:24:16 +0900

    Time: 22 ms (47.46%), Space: 47.3 MB (19.57%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.kt
+++ 0383-ransom-note/0383-ransom-note.kt
@@ -1,16 +1,18 @@
 class Solution {
     fun canConstruct(ransomNote: String, magazine: String): Boolean {
-        val result = mutableMapOf<Char, Int>()
+        //magazine에 있는 문자들로 ransomNote를 만들수 있냐 없냐
+        val result = mutableMapOf<Char,Int>()
 
         for(i in magazine){
             result[i] = result.getOrDefault(i,0)+1 
+            // result라는 map에다가 magazine의 문자인 i가있는지, 없으면 1추가
         }
         for(j in ransomNote){
-            if(result.getOrDefault(j,0) == 0){
+            if(result.getOrDefault(j,0)==0){
                 return false
             } 
-            result[j]=result[j]!!-1
+            result[j] = result[j]!!-1
         } 
-        return true 
+        return true
     }
-}
+} 
