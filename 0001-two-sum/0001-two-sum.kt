bec3b96a8df732eb5c8a6ea61c7a9afb5c4f27cd
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-11 15:07:58 +0900

    Time: 3 ms (62.45%), Space: 48.3 MB (28.66%) - LeetHub

--- 0001-two-sum/0001-two-sum.kt
+++ 0001-two-sum/0001-two-sum.kt
@@ -1,12 +1,13 @@
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        val map = mutableMapOf<Int, Int>()
-
-        for (i in nums.indices) {
+        val map = mutableMapOf<Int,Int>()
+        //target에서 0번인덱스부터 필요한 짝 찾기
+        for(i in nums.indices){
             val needed = target - nums[i]
-            if (map.containsKey(needed)) {
+        
+            if(map.containsKey(needed)){
                 return intArrayOf(map[needed]!!, i)
-            }
+            } 
             map[nums[i]] = i
         }
         return intArrayOf()
