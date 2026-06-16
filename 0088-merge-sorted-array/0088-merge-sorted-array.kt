75522b93e7eeb39d8788f67f7ce79c8726dae04a
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-16 13:18:13 +0900

    Time: 8 ms (18.96%), Space: 45.4 MB (15.9%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.kt
+++ 0088-merge-sorted-array/0088-merge-sorted-array.kt
@@ -1,13 +1,12 @@
 class Solution {
     fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
-        if(n == 0){
-            return
-        } 
-        var pointer = 0
-        for(i in m until m+n){
-            nums1[i] = nums2[pointer]
-            pointer++
-        }  
-        nums1.sort()
+        //length = m+n
+        //점점 증가
+        var zero = 0
+        for(i in m until m+n){      
+                    nums1[i] = nums2[zero]
+                        zero++     
+        }
+        nums1.sort() 
     }
 }
