d7ca266ff04a494662db33152db3a079fc417efb
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-16 13:02:53 +0900

    Time: 1 ms (91.19%), Space: 55.4 MB (50.45%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.kt
+++ 0344-reverse-string/0344-reverse-string.kt
@@ -1,18 +1,16 @@
 class Solution {
     fun reverseString(s: CharArray): Unit {
-        if(s.size == 1){
-            return 
-        } 
-        var left = 0 
-        var right = s.size - 1  
-        while(left < right){
+        var left = 0
+        var right = s.size-1 
+
+        while(left<right){
             val temp = s[left]
             s[left] = s[right]
             s[right] = temp
-            left++
-            right--
-        }
+
+            left ++
+            right --
+        } 
         return
-        
     }
 }
