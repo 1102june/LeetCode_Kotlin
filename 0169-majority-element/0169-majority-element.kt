f93afe2d5b6776424bc502adff928c1af637535f
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-17 14:11:25 +0900

    Time: 16 ms (31.74%), Space: 53.6 MB (57.19%) - LeetHub

--- 0169-majority-element/0169-majority-element.kt
+++ 0169-majority-element/0169-majority-element.kt
@@ -1,13 +1,16 @@
 class Solution {
     fun majorityElement(nums: IntArray): Int {
-        val map = mutableMapOf<Int, Int>()
+        var map = mutableMapOf<Int, Int>()
+
         for(i in nums){
             val count = map.getOrDefault(i,0)+1
             map[i] = count
-            if(count*2>nums.size){
+
+            if(count*2 > nums.size){
                 return i
-            }  
-        } 
-        return -1
+            }
+        }
+        return -1 
+        
     }
 }
