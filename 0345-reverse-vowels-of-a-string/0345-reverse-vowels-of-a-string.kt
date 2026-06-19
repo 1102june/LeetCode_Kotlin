0ae4d046567d154142e7e5d68b485f1c6a4cbfec
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-19 13:46:46 +0900

    Time: 10 ms (91.93%), Space: 46.6 MB (82.11%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.kt
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.kt
@@ -1,29 +1,31 @@
 class Solution {
     fun reverseVowels(s: String): String {
         val vowels = "aeiouAEIOU"
-        var left = 0
+        val arr= s.toCharArray()
+        var left = 0 
         var right = s.length-1
-        var a = s.toCharArray()
-
-        if(s.length == 1){  //예외처리
-            return s
-        }
         while(left < right){
-            if(a[left] !in vowels){
-                left++
-            }
-            else if(a[right] !in vowels){
-                right--
-            }  
-            if(a[left] in vowels && a[right] in vowels){
-                val temp = a[left]
-                a[left] = a[right]
-                a[right] = temp
+            val a = arr[left] in vowels
+            val b = arr[right] in vowels
 
-                right--
+            if(a==true && b==true){
+                val temp = arr[left]
+                arr[left] = arr[right]
+                arr[right] = temp
                 left++
+                right--
             }
-        }  
-        return String(a)
+            if (a==true && b==false){
+                right--
+            } 
+            if (a==false && b==true){
+                left++
+            } 
+            if (a==false && b==false){
+                left++
+                right--
+            } 
+        } 
+    return String(arr)
     }
 }
