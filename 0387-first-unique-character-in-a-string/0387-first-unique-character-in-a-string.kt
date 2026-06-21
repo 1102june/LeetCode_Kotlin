65027f278d5fd54af4c92820b13f0ecab6352a6a
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-21 14:32:05 +0900

    Time: 59 ms (8.12%), Space: 47.7 MB (26.5%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.kt
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.kt
@@ -1,14 +1,12 @@
 class Solution {
     fun firstUniqChar(s: String): Int {
-        // 일단 문자 개수대로 map만들기
-        val map = s.groupingBy{it}.eachCount()  //개수대로 map만들었음
-        
-        for(i in 0 until s.length){
+        val map = s.groupingBy{it}.eachCount()
+
+        for(i in s.indices){
             if(map[s[i]] == 1){
                 return i
             } 
-        } 
-
+        }
         return -1
     }
 }
