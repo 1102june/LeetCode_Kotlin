aafa5a64109de19ba56fff54eabb48e57de13687
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-22 12:11:27 +0900

    Time: 20 ms (39.18%), Space: 45.6 MB (58.31%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.kt
@@ -0,0 +1,24 @@
+class Solution {
+    fun isValid(s: String): Boolean {
+        val stack = ArrayDeque<Char>()
+
+        for(i in s){
+            if(i == '(' ||i =='[' || i == '{' ){
+                stack.add(i)
+            }
+            else {
+                if(stack.isEmpty()){
+                    return false
+                } 
+                val top = stack.removeLast()
+                if ((i == ')' && top != '(') || 
+                    (i == ']' && top != '[') || 
+                    (i == '}' && top != '{')) {
+                    return false
+                }
+            } 
+        }    
+    
+    return stack.isEmpty()
+    }
+}
