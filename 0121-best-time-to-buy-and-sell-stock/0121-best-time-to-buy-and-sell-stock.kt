c91c791b10a6ec3dafefcd0797962348e2dd888b
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-22 11:27:44 +0900

    Time: 3 ms (86.63%), Space: 65.6 MB (48.97%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.kt
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.kt
@@ -1,16 +1,16 @@
 class Solution {
     fun maxProfit(prices: IntArray): Int {
-        var min = 10000
         var max = 0
+        var min = 100000
 
         for(i in prices){
             if(i < min){
-                min = i // 최소값 갱신
+                min = i
             } 
-            else if(i-min > max){
-                max = i - min 
+            if(i - min> max){
+                max = i - min
+            } 
         }
-    }
-    return max 
+        return max 
     }
 }
