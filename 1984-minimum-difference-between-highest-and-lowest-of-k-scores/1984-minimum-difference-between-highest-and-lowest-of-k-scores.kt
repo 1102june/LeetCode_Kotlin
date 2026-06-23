05c65dfe7e573669fb8099232a8ccd909adee78c
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-23 15:46:59 +0900

    Time: 12 ms (100%), Space: 49.2 MB (72.73%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.kt
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.kt
@@ -1,17 +1,15 @@
 class Solution {
     fun minimumDifference(nums: IntArray, k: Int): Int {
-        if(nums.size == 1 || k == 1){
-            return 0
-        }
         var result = 100000
-        nums.sort()  // sorted()는 새로운 함수 반환
-        for(i in 0 until nums.size - k+1){
-            val max = nums[i+k-1]
-            val min = nums[i]
-            if(max - min < result){
+        nums.sort()
+
+        for(i in 0 until nums.size-k+1){
+            var min = nums[i]
+            var max = nums[i+k-1]
+            if(max-min < result){
                 result = max-min
             }
         } 
-        return result
+        return result    
     }
 }
