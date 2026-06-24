d0e0d137bcaf27601e0d87c6be17db4c26c9aec7
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-24 18:39:19 +0900

    Time: 0 ms (100%), Space: 44.3 MB (30.61%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun searchInsert(nums: IntArray, target: Int): Int {
+        var low = 0
+        var high = nums.size-1
+
+        while(low <= high){
+            val mid = (low+high)/2
+            if(nums[mid] == target){
+                return mid
+            }
+            else if(nums[mid] > target){
+                high = mid -1
+            }
+            else if(nums[mid] < target) {
+                low = mid + 1
+            }      
+        }
+        return low
+    }
+}
