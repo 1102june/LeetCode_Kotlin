b088ff181e5001b1f17881d85ae93cc54de1bcdf
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-24 17:10:57 +0900

    Time: 23 ms (5.26%), Space: 46.9 MB (12.28%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.kt
+++ 0682-baseball-game/0682-baseball-game.kt
@@ -1,29 +1,28 @@
 class Solution {
     fun calPoints(operations: Array<String>): Int {
+        //+는 앞에 두개 더한뒤 push
+        //D는 앞에 점수 두배한뒤 push
+        //C는 앞에 점수 삭제
         val stack = ArrayDeque<Int>()
         var result = 0
-        // C는 pop 1회
-        // D는 pop 한뒤 x2 해서 push
-        // +는 pop 2개한뒤 + 해서 push
 
         for(i in 0 until operations.size){
-                if(operations[i] == "C"){
-                    stack.removeLast() // pop 1회
-                    }
-                else if(operations[i]  == "D"){
-                    stack.add(stack.last() * 2)  //stack.last()는 제거안함. 
-                    }
-                else if(operations[i] == "+"){
-                    stack.add(stack[stack.size-1] + stack[stack.size-2]) 
-                }
-                else{
-                stack.add(operations[i].toInt())
-                    }  
+            if(operations[i] == "+"){
+                stack.add(stack[stack.size-1] + stack[stack.size-2])
             } 
-
+            else if(operations[i] == "D"){
+                stack.add(stack.last()*2)
+            }
+            else if(operations[i] == "C"){
+                stack.removeLast()
+            }
+            else{
+                stack.add(operations[i].toInt())
+            }  
+        }
         for(j in stack){
             result+=j
         } 
-        return result    
-    }   
+        return result
+    }
 }
