8a38780673ee4666b92db13d6a85e6437e8562a7
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-26 13:37:23 +0900

    Time: 0 ms (100%), Space: 49.2 MB (74.36%) - LeetHub

--- 0704-binary-search/0704-binary-search.kt
+++ 0704-binary-search/0704-binary-search.kt
@@ -1,21 +1,20 @@
 class Solution {
     fun search(nums: IntArray, target: Int): Int {
-        var low = 0
-        var high = nums.size-1
-
-        while(low <= high){
-            val mid = (low+high)/2
+        var left = 0
+        var right = nums.size-1
+        
+        while(left <= right){
+            var mid = (left + right) / 2
             if(nums[mid] == target){
                 return mid
             } 
-            else if(nums[mid] > target){
-                high = mid -1 
+            else if(nums[mid] < target){
+                left = mid+1
             }
-            else{
-                low = mid + 1
-            } 
+            else if(nums[mid] > target){
+                right = mid-1
+            }  
         }
-        return -1 
-        
+        return -1
     }
 }
