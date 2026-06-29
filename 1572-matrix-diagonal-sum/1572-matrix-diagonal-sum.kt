d179613554a3aef06d83da85dd8b157be01cb334
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-29 18:49:00 +0900

    Time: 0 ms (100%), Space: 47.3 MB (72.73%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.kt
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.kt
@@ -1,23 +1,21 @@
 class Solution {
     fun diagonalSum(mat: Array<IntArray>): Int {
         val a = mat.size
-        val b = mat[0].size
- 
+        val b = mat[0].size 
         var result = 0
-        if(a==1 && b==1){
+
+        if(a==1&&b==1){
             return mat[0][0]
         } 
-        // primary diagonal 
-        for(i in 0 until a){
-            result+=mat[i][i]
 
+        for(i in 0 until a){
+            //primary diagonal
+            result+=mat[i][i]
             result+=mat[i][a-1-i]
-        } 
-        // secondary diagonal
-        
-        if(a%2==1){  //홀수X홀수의 2차원배열인 경우 중앙값 빼버리기
-            result-=mat[a/2][b/2]
         }
-        return result        
+        if(a%2==1){
+            result -=mat[a/2][b/2]
+        }
+        return result  
     }
 }
