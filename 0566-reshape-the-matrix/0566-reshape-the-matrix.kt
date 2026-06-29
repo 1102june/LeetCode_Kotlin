77220c8426b74111328ac4e0dc83a54ad15ffd16
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-29 18:44:29 +0900

    Time: 0 ms (100%), Space: 46 MB (90.91%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.kt
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.kt
@@ -1,20 +1,19 @@
 class Solution {
     fun matrixReshape(mat: Array<IntArray>, r: Int, c: Int): Array<IntArray> {
-        val a = mat.size 
+        val a = mat.size
         val b = mat[0].size
-
-        val arr = Array(r){IntArray(c)}
-        var count = 0
-
-        if(r*c != a*b){
+        val arr = Array(r) {IntArray(c)}
+        var count =0
+        if(a*b != r*c){
             return mat
-        }
+        } 
+
         for(i in 0 until a){
             for(j in 0 until b){
                 arr[count/c][count%c] = mat[i][j]
                 count++
-            } 
-        } 
-        return arr
+            }
+        }
+        return arr 
     }
 }
