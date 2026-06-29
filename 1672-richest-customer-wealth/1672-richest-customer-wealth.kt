cd9ccdd292ed8f1e7808d28a57665cc73c3c527f
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-29 18:35:40 +0900

    Time: 0 ms (100%), Space: 42.9 MB (81.43%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.kt
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.kt
@@ -2,18 +2,18 @@ class Solution {
     fun maximumWealth(accounts: Array<IntArray>): Int {
         val a = accounts.size
         val b = accounts[0].size
-
+        
         var result = 0
 
         for(i in 0 until a){
-            var sum = 0 
+            var sum = 0
             for(j in 0 until b){
-                sum += accounts[i][j]
-            }
-            if(result < sum){
+                sum+= accounts[i][j]
+            } 
+            if(sum> result){
                 result = sum
             } 
         }
-        return result   
+        return result 
     }
 }
