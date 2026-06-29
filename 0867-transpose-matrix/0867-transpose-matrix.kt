2477b409d81030286e5a1d38dd5e0b6d7464368f
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-29 18:38:22 +0900

    Time: 0 ms (100%), Space: 48.7 MB (21.95%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.kt
+++ 0867-transpose-matrix/0867-transpose-matrix.kt
@@ -2,7 +2,7 @@ class Solution {
     fun transpose(matrix: Array<IntArray>): Array<IntArray> {
         val a = matrix.size
         val b = matrix[0].size
-        val arr = Array(b) {IntArray(a)}
+        val arr = Array(b) {IntArray(a)} 
 
         for(i in 0 until a){
             for(j in 0 until b){
