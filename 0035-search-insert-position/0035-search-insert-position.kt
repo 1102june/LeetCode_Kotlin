db10c3185abe8017af7f7e136373aad229f2e9d0
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-29 18:30:41 +0900

    Time: 0 ms (100%), Space: 44.8 MB (25.1%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.kt
+++ 0035-search-insert-position/0035-search-insert-position.kt
@@ -3,18 +3,18 @@ class Solution {
         var left = 0
         var right = nums.size-1
 
-        while(left <= right){
-            val mid = (left+right) / 2
+        while(left <= right) {
+            val mid = (left+right) /2
             if(nums[mid] == target){
                 return mid
             } 
-            else if(nums[mid] < target){
-                left = mid + 1
+            if(nums[mid] < target){
+                left = mid +1
             } 
-            else if(nums[mid] > target){
-                right = mid - 1
-            } 
-        }
+            if(nums[mid] > target){
+                right = mid -1
+            }
+        } 
         return left 
     }
 }
