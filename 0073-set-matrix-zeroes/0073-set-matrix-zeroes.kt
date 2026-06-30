17f52d74b8dd696b78062506ae0655f2da4dc246
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-30 17:11:50 +0900

    Time: 1 ms (100%), Space: 49.3 MB (83.93%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.kt
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.kt
@@ -3,26 +3,25 @@ class Solution {
         val a = matrix.size
         val b = matrix[0].size
 
-        var c = BooleanArray(a) //크기가 a인 불대수 배열 
-        var d = BooleanArray(b) // [false, false, false....]
-        
-        // 0 을 찾아라
+        val c = BooleanArray(a)
+        val d = BooleanArray(b)
+
+        // 0 찾아서 메모
         for(i in 0 until a){
             for(j in 0 until b){
                 if(matrix[i][j] == 0){
                     c[i] = true
                     d[j] = true
-                } 
+                }
             } 
         } 
-        // 불대수 배열에서 true라고 되어있는곳 0으로 덮기
+        // 이제 0인 행,열에다가 0박아넣기
         for(i in 0 until a){
             for(j in 0 until b){
                 if(c[i] || d[j]){
                     matrix[i][j] = 0
-                } 
+                }
             } 
-        } 
-        
+        }
     }
 }
