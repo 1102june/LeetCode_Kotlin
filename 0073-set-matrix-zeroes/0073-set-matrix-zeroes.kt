baec36d2e15a4ad10b30ae4ccd8a371243d23066
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-30 17:05:41 +0900

    Time: 1 ms (100%), Space: 48.4 MB (99.4%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.kt
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.kt
@@ -1,31 +1,28 @@
 class Solution {
     fun setZeroes(matrix: Array<IntArray>): Unit {
-        // 1,1 이 0이라면
-        // 일단 같은 열은 무조건 다 0
-        // 그 행도 다 0
-        // 0을 만나면 그 모든 행,열 0으로 만들고 넘어가기
         val a = matrix.size
         val b = matrix[0].size
 
-        val rowMemo = BooleanArray(a)
-        val colMemo = BooleanArray(b)
-
-// 먼저 0의 위치를 기억해두기 
+        var c = BooleanArray(a) //크기가 a인 불대수 배열 
+        var d = BooleanArray(b) // [false, false, false....]
+        
+        // 0 을 찾아라
         for(i in 0 until a){
             for(j in 0 until b){
                 if(matrix[i][j] == 0){
-                    rowMemo[i] = true
-                    colMemo[j] = true
-                    } 
+                    c[i] = true
+                    d[j] = true
                 } 
-            }  
-        
+            } 
+        } 
+        // 불대수 배열에서 true라고 되어있는곳 0으로 덮기
         for(i in 0 until a){
             for(j in 0 until b){
-                if(rowMemo[i] || colMemo[j]){
-                    matrix[i][j]=0
+                if(c[i] || d[j]){
+                    matrix[i][j] = 0
                 } 
-            }  
-        }
+            } 
+        } 
+        
     }
 }
