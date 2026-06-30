bcf60d82867a62f012ac58eb50f24c82ff4bda1f
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-06-30 17:22:17 +0900

    Time: 10 ms (28.12%), Space: 45.3 MB (24.72%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.kt
+++ 0048-rotate-image/0048-rotate-image.kt
@@ -1,18 +1,17 @@
 class Solution {
     fun rotate(matrix: Array<IntArray>): Unit {
+        // reverse 할 생각을 할 수 있는가?
+        matrix.reverse()
         val a = matrix.size
         val b = matrix[0].size
-        //어차피 n x n 행렬이라 둘다 선언 안해도되긴함
-        
-        matrix.reverse() // kotlin reverse funtions
 
         for(i in 0 until a){
             for(j in i+1 until b){
                 val temp = matrix[i][j]
                 matrix[i][j] = matrix[j][i]
                 matrix[j][i] = temp
-            }
-        }
-
+            } 
+        } 
+        
     }
 }
