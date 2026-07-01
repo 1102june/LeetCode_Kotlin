b8ad078718be3b0ca065e1c50a0309753c2aa598
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-07-01 12:47:11 +0900

    Time: 1 ms (100%), Space: 55.1 MB (38.69%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.kt
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.kt
@@ -6,22 +6,21 @@ class Solution {
         val c = BooleanArray(a)
         val d = BooleanArray(b)
 
-        // 0 찾아서 메모
         for(i in 0 until a){
             for(j in 0 until b){
                 if(matrix[i][j] == 0){
                     c[i] = true
                     d[j] = true
-                }
+                } 
             } 
         } 
-        // 이제 0인 행,열에다가 0박아넣기
+        
         for(i in 0 until a){
             for(j in 0 until b){
                 if(c[i] || d[j]){
                     matrix[i][j] = 0
-                }
-            } 
+                } 
+            }
         }
     }
 }
