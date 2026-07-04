19d138ee57a74b3a672d20c249768ea05e7febf6
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-07-04 15:01:55 +0900

    Time: 248 ms (5.02%), Space: 47.4 MB (43.1%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.kt
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.kt
@@ -4,14 +4,14 @@ class Solution {
 
         for(i in 0 until s.length){
             val ch1 = s[i]
-            map[ch1] = map.getOrDefault(ch1,0)+1  
-        }
+            map[ch1] = map.getOrDefault(ch1, 0)+1 
+        } 
         for(j in 0 until s.length){
             val ch2 = s[j]
             if(map[ch2] == 1){
                 return j
             } 
-        }  
-        return -1
+        }
+        return -1 
     }
 }
