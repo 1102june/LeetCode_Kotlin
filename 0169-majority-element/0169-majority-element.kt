2650159b1afca53b4f594756481f5896bca0912b
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-07-07 18:33:51 +0900

    Time: 20 ms (19.02%), Space: 52.4 MB (86.44%) - LeetHub

--- 0169-majority-element/0169-majority-element.kt
+++ 0169-majority-element/0169-majority-element.kt
@@ -1,18 +1,18 @@
 class Solution {
     fun majorityElement(nums: IntArray): Int {
-        val map = HashMap<Int,Int>()
+        val map = HashMap<Int, Int>()
 
         for(i in 0 until nums.size){
             val a = nums[i]
             map[a] = map.getOrDefault(a,0)+1
-        } 
+        }
 
         for(j in 0 until nums.size){
             val b = nums[j]
             if(map[b]!! > (nums.size/2)){
-                return nums[j]
+                return b
             } 
-        } 
-        return  -1
+        }
+        return -1  
     }
 }
