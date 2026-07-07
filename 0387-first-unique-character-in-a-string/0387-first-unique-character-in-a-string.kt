675d43a8d6c68ac1a55ca42c2cc5f0079537c566
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-07-07 18:31:20 +0900

    Time: 42 ms (41.6%), Space: 46.7 MB (74.37%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.kt
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.kt
@@ -6,13 +6,12 @@ class Solution {
             val a = s[i]
             map[a] = map.getOrDefault(a,0)+1
         } 
-        //2차 순회하면서 map의 value가 1인값 찾으면 바로 해당 index return
         for(j in 0 until s.length){
             val b = s[j]
             if(map[b] == 1){
                 return j
             } 
-        }
-        return -1 
+        } 
+        return -1
     }
 }
