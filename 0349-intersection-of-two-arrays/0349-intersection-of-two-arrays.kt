49507e8bfd8f189d04cddd54dde94b9099020dd6
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-07-08 13:54:07 +0900

    Time: 22 ms (35.64%), Space: 47.4 MB (8.91%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.kt
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.kt
@@ -1,15 +1,13 @@
 class Solution {
     fun intersection(nums1: IntArray, nums2: IntArray): IntArray {
-        val result = mutableListOf<Int>() // arrayListOf<Int>()
-        
+        val result = mutableListOf<Int>()
         val nums3 = nums1.toSet()
         val nums4 = nums2.toSet()
-        
+
         for(i in nums3){
             if(i in nums4){
                 result.add(i)
             } 
-
         } 
         return result.toIntArray()
     }
