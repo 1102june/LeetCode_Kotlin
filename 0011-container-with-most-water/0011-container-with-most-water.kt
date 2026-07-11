110bf2e76514801eb6032437e484915076bfc661
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-07-11 16:58:14 +0900

    Time: 5 ms (93.46%), Space: 59.8 MB (66.85%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.kt
+++ 0011-container-with-most-water/0011-container-with-most-water.kt
@@ -1,25 +1,20 @@
 class Solution {
     fun maxArea(height: IntArray): Int {
-        if(height.size == 2){
-            return minOf(height[0],height[1])
-        }
-        var max = 0
         var left = 0
         var right = height.size-1
-
+        var result = 0
         while(left < right){
-            val sum = (right-left) * (minOf(height[left], height[right]))
-            if(sum > max){
-                max = sum
-            }
-            // left, right pointer를 어떻게 움직일것인가
-            if(height[left] < height[right]){
+            val sum = (right - left) * minOf(height[left], height[right])
+            if(result < sum){
+                result = sum
+            } 
+            if(height[left] < height[right]){ //벽끼리 높이 비교해서 더 짧은쪽 포인터 버리기
                 left++
             }
-            else{
+            else {
                 right--
             }
-        } 
-        return max
+        }
+        return result
     }
 }
