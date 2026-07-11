da8e87f258b96923a573925d67643f6c8ef64e80
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-07-11 18:56:35 +0900

    Time: 8 ms (85.21%), Space: 43.7 MB (95.64%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.kt
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.kt
@@ -2,20 +2,23 @@ class Solution {
     fun lengthOfLongestSubstring(s: String): Int {
         val result = mutableListOf<Char>()
 
-        var left = 0  // left는 규칙위반시(새로 들어오는 애랑 중독되면) result에서 제거함
+        var left = 0
+        var right = 0
         var max = 0
 
-        for(right in 0 until s.length){
-            //right는 그냥 전진
-            while(s[right] in result){
+        while(right < s.length) {
+            if(s[right] in result){
                 result.remove(s[left])
-                left++    
-                }
-            result.add(s[right])
-            if(result.size > max){
+                left++
+            }
+            else{
+                result.add(s[right])
+                right++
+            }
+            if(max < result.size){
                 max = result.size
-            }            
-        } 
+            }   
+        }
         return max
     }
 }
