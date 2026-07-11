ba881e89cac3d7b3b7dec45db2405c2ee0d593b8
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-07-11 16:46:37 +0900

    Time: 2 ms (96.79%), Space: 50.2 MB (71.57%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.kt
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.kt
@@ -1,18 +1,18 @@
 class Solution {
     fun twoSum(numbers: IntArray, target: Int): IntArray {
         val result = IntArray(2)
+
         if(numbers.size == 2){
             result[0] = 1
             result[1] = 2
-            return result
-        } 
+        }
         var left = 0
         var right = numbers.size -1
         while(left < right){
             val sum = numbers[left] + numbers[right]
             if(sum < target){
                 left++
-            }  
+            } 
             else if(sum > target){
                 right--
             }
@@ -20,8 +20,9 @@ class Solution {
                 result[0] = left+1
                 result[1] = right+1
                 break
-            } 
-        } 
-        return result
+            }  
+
+        }
+            return result
     }
 }
