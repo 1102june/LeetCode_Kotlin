6e24bf65c075344605871a40d0b0fc73c8e18b22
SEO YAE JUNE <162932571+1102june@users.noreply.github.com>
2026-07-15 15:13:35 +0900

    Time: 59 ms (60.96%), Space: 61.9 MB (82.87%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.kt
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.kt
@@ -2,24 +2,22 @@ class Solution {
     fun longestConsecutive(nums: IntArray): Int {
         if(nums.size == 1){
             return 1
-        } 
-        //set, sort하고 다음인덱스랑 1차이나면 result++
-        //O(n) time, sorted 하는순간부터 O(NlogN)
-
-        val set = nums.toSet() // 중복제거, O(N)
-        var maxStreak = 0 
+        }
+        val set = nums.toSet() // toSet() -> O(n) 
+        var max = 0
 
         for(i in set){
-            if(!set.contains(i-1)){  //맨앞칸이 아니라면. 결국 시작점 찾기
-                var currNum = i
-                var currStreak = 1  //기차 길이가 1로시작함. 
-                while(set.contains(currNum +1)){ 
-                    currNum++
-                    currStreak++
-                }
-                maxStreak = maxOf(maxStreak, currStreak)
+            if(!set.contains(i-1)){ // 만약 이게 시작점이면 
+                var curr = i
+                var currMax = 1
+            
+            while(set.contains(curr+1)){
+                curr++
+                currMax++
+            }
+            max = maxOf(max, currMax)
             }
         }
-        return maxStreak
+        return max  
     }
 }
